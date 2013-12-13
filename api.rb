bbcba7032633b103ba8dcfc8671b1fac3f5f3220
Lucas <li_kaseta@hotmail.com>
2013-12-13 14:40:50 -0300

    ..

--- api.rb
+++ api.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'jbuilder'
 
 class Resource < ActiveRecord::Base; end
 
@@ -20,7 +21,37 @@ end
 get '/resources' do
   content_type :json
   #aca devuelvo las cosas 
-  {:resources => Resource.all}.to_json
+  resources = Resource.all
+	Jbuilder.encode do |json|
+
+
+		json.resources do
+		  json.array! @resources do |r|
+		    json.name r.name
+		    json.description r.description
+		  end
+		end
+  end
+
+end
+
+get '/prueba' do
+  content_type :json
+  h= {resources: []}
+  Resource.all.each do |e|
+  	resource = {
+  		id: e.id,
+  		name: e.name,
+  		description: e.description,
+  		links: [{
+  			rel: 'self',
+  			uri: 'http://localhost.com'
+  		}
+  		]
+  	}
+  	h[:resources].push(resource)
+  end 
+  h.to_json
 end
 
 not_found do
